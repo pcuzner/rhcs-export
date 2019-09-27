370dd8cfb5a6007e7414881e9d57f1282e39b9eb
Paul Cuzner <pcuzner@redhat.com>
2019-09-27 12:29:24 +1200

    Switched to use ceph -s as the main data source
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- README.md
+++ README.md
@@ -8,33 +8,35 @@ This is a simple python script, so could be installed directly by copying the fi
 ## How to Use it
 The ceph export command should be installed in a mgr or mon node. Once installed the command provides the following options;
 ```
-[root@ceph-mgr ~]$ ceph-export.py -h
-usage: ceph-export.py [-h] [-o OUTPUT] [-c CONFDIR] [-u USER]
+[root@ceph-mgr ~]$ python ceph-export.py -h                    
+usage: ceph-export.py [-h] [-o OUTPUT] [-c CONFDIR] [-u USER] [-f {yaml}]
 
 Export RHCS configuration information
 
 optional arguments:
-  -h, --help            show this help message and exit
+  -h, --help            show this help message and exit                  
   -o OUTPUT, --output OUTPUT
-                        output file for the export (default: ~/rhcs-config-
-                        export.yaml)
+                        output file for the export (default: ~/rhcs-config-export.yaml)
   -c CONFDIR, --confdir CONFDIR
                         ceph configuration directory (default: /etc/ceph)
   -u USER, --user USER  Ceph user to use for the keyring (default: admin)
-
+  -f {yaml}, --format {yaml}
+                        output file format (default: yaml)
 ```
+
 ### Example output
 Here's an example of the file the tool creates
 ```
 ---
+dashboard: http://rhcs4-2.storage.lab:8443/
 fsid: 6d210768-d391-409b-b585-56d54554da8c
-mon_host: 10.90.90.160
-secret: AQCrGYBdRH3XLRAA+LojQqElDRXHL6FIb5QvXg==
+mgr: 10.90.90.161
+mons: [10.90.90.153, 10.90.90.160, 10.90.90.161]                                             
+rgws: ['10.90.90.153:8080']
+secret: AQCrGYBdRH3XLRAA+LojQqElDRXHL6FIb5QvXg== 
 ```
   
 ## TODO
-* Add mgr/dashboard info to provide the ability for the import process to setup link-n-launch
 * add ceph version
-* add service layout information
 * support yaml/bash output formats (yaml only currently)
 * complete packaging
--- ceph-export.py
+++ ceph-export.py
@@ -1,7 +1,10 @@
 import os
 import sys
 import yaml
+import json
+import shutil
 import argparse
+import subprocess
 import configparser
 
 KEYRING_FILE = 'ceph.client.{}.keyring'
@@ -11,9 +14,16 @@ class Defaults(object):
     user = "admin"
     conf_dir = '/etc/ceph'
     output = '~/rhcs-config-export.yaml'
+    output_format = 'yaml'
+
+
+class Choices(object):
+    output_format = ['yaml']
 
 
 def ready():
+    if not shutil.which('ceph'):
+        return False, "ceph command not found!"
     if not os.path.isfile(os.path.join(args.confdir, 'ceph.conf')):
         return False, "ceph.conf not found"
     if not os.path.isfile(os.path.join(args.confdir, 
@@ -57,6 +67,35 @@ def abort(message=None):
 def help():
     print("help me...")
 
+def send_command(cmd):
+    cmd_stream = cmd.split(' ')
+    _c = subprocess.Popen(cmd_stream,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT)
+    return _c.communicate()
+
+def write_file(content):
+    
+    try:
+        with open(os.path.expanduser(args.output), 'w') as f:
+            f.write(content)
+    except:
+        print("Unexpected problem writing the file, dumping config here")
+        print(output)
+    else:
+        print("Config written to {}".format(os.path.expanduser(args.output)))
+
+def _dump_yaml(settings):
+    
+    output = yaml.safe_dump(settings, indent=2, explicit_start=2)
+    write_file(output)
+
+def dump(settings):
+    if args.format == 'yaml':
+        _dump_yaml(settings)
+    else:
+        abort("Unknown output format requested")    
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Export RHCS configuration information",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -66,6 +105,10 @@ def parse_args():
                         default=Defaults.conf_dir, help="ceph configuration directory")
     parser.add_argument("-u", "--user", type=str, required=False,
                         default=Defaults.user, help="Ceph user to use for the keyring")
+    parser.add_argument("-f", "--format", type=str, required=False,
+                        choices=Choices.output_format,
+                        default=Defaults.output_format, 
+                        help="output file format")
     return parser.parse_args()
 
 def main():
@@ -75,29 +118,46 @@ def main():
         abort(_err)
 
     conf = get_config(os.path.join(args.confdir, 'ceph.conf'))
-    if not all([i in conf['global'].keys() for i in ['fsid', 'mon host']]):
-        abort("Invalid ceph.conf file detected. Missing fsid and/or mon host")
-
     keyring = find_keyring()
     if not keyring:
         abort("Can't find a keyring to use")
 
+    print("Fetching state of the local Ceph cluster")
+    ceph_out, error = send_command('ceph -s -f json')
+    if error:
+        abort("ceph command failed - can't determine state")
+    
+    ceph_version_text, error = send_command('ceph --version')
+    if error:
+        abort("unable to fetch ceph version!")
+
+    ceph_state = json.loads(ceph_out)
+
     settings = dict()
-    mon_host_str = conf['global']['mon host']
-    settings['fsid'] = conf['global']['fsid']
-    settings['mon_host'] = mon_host_str.split(',')[0].split(':')[1]
+
+    settings['fsid'] = ceph_state['fsid']
+    settings['version'] = str(ceph_version_text).split()[2].split('-')[0]  # just want a V.R.M for the version id
     settings['secret'] = keyring
+    settings['mons'] = [mon['public_addr'].split(':')[0] for mon in ceph_state['monmap']['mons']]
+    settings['mgr'] = ceph_state['mgrmap']['active_addr'].split(':')[0]
+   
+    dashboard_url = ceph_state['mgrmap']['services'].get('dashboard', None)
+    if dashboard_url:
+        settings['dashboard'] = dashboard_url
+   
+    if ceph_state.get('servicemap', None):
+        if ceph_state['servicemap']['services'].get('rgw', None):
+            daemons = ceph_state['servicemap']['services']['rgw']['daemons']
+            rgws = list()
+            for rgw in daemons:
+                if rgw == "summary":
+                    continue
+                frontend_str = daemons[rgw]['metadata']['frontend_config#0']
+                rgws.append(frontend_str.split('port=')[1].split()[0])
+            settings['rgws'] = rgws
+        
+    dump(settings)
 
-    output = yaml.safe_dump(settings, indent=2, explicit_start=2)
-
-    try:
-        with open(os.path.expanduser(args.output), 'w') as f:
-            f.write(output)
-    except:
-        print("Unexpected problem writing the file, dumping config here")
-        print(output)
-    else:
-        print("Config written to {}".format(os.path.expanduser(args.output)))
 
 if __name__ == '__main__':
     args = parse_args()
